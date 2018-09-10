ff20c074f73e8760fa1fa59e00d73de4f6a02acc
dhaneshande <dande@miraclesoft.com>
2018-09-11 00:17:01 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './Components/app-component/app.component';
-
+//comments
 @NgModule({
   declarations: [
     AppComponent
